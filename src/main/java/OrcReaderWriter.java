e535defc2bc07ed4bd1bfd6fe9462c0ae1eee9b3
Rakesh Das <rakesh.rasen@gmail.com>
2019-07-21 10:55:01 +0530

    Update OrcReaderWriter.java

--- src/main/java/OrcReaderWriter.java
+++ src/main/java/OrcReaderWriter.java
@@ -18,12 +18,12 @@ public class OrcReaderWriter {
         System.out.println("Time Elapsed in seconds : " + stopWatch.getTime() / 1000.0);
 
         //===========Writer============================
-        /*String writeLocation="path-where-you-want-to-write";
+        String writeLocation="path-where-you-want-to-write";
         File file = new File(writeLocation);
         boolean fileExists = file.exists();
         if(fileExists) file.delete();
         System.out.println("File deleted");
         Path path = new Path(writeLocation);
-        orcUtilities.WriteOrcFile(path,conf);*/
+        orcUtilities.WriteOrcFile(path,conf);
     }
 }
